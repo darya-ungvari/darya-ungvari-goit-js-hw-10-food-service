ac3a6ac3de76f516faff8af09645454c335a6695
darya-ungvari <63539808+darya-ungvari@users.noreply.github.com>
2020-10-21 16:29:33 +0300

    delete express

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+// {
+//     module: {
+//       rules: [
+//         { test: /\.hbs$/, exclude: /node_modules/, use: "handlebars-loader" }
+//       ]
+//     }
+//   }
+
+  const path = require('path');
+
+module.exports = {
+    entry: './javascripts/index.js',
+    output: {
+    filename: 'mybundle.js',
+    path: path.resolve(__dirname, 'public'),
+  }
+};
